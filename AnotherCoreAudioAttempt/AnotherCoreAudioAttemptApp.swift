210f6dc4cec1aa23b910d50d002e2b05f1411d91
Kody Deda <kodydeda4@gmail.com>
2021-01-22 09:34:40 -0500

    Initial Commit

---
+++ AnotherCoreAudioAttempt/AnotherCoreAudioAttemptApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherCoreAudioAttemptApp.swift
+//  AnotherCoreAudioAttempt
+//
+//  Created by Kody Deda on 1/22/21.
+//
+
+import SwiftUI
+
+@main
+struct AnotherCoreAudioAttemptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnotherCoreAudioAttempt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnotherCoreAudioAttempt
+//
+//  Created by Kody Deda on 1/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
